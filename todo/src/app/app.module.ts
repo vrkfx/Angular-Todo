0f22fb2b9f9b794a6bd0f4f3141dcb13805a4ec4
Virroshan_Kumar <virroshan08@outlook.com>
2023-01-28 12:04:38 +1030

    initial Set up with 2 components, routing and Angular material,  Form Modules

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// RouterModules
+import { Routes, RouterModule } from '@angular/router';
+
 //Angular Material
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatFormFieldModule} from '@angular/material/form-field'
+
+//Components
+import { TodoComponent } from './todo/todo.component';
+import { HomeComponent } from './home/home.component';
+
+//Form Modules
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+const routes: Routes=[
+  { path:'todo', component: TodoComponent },
+  //Path Match Can be 'full' or prefix
+  { path: '', redirectTo: '/todo', pathMatch: 'full' },
+  { path:'', component: TodoComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
+    //Material UI test
     MatSlideToggleModule,
+    MatFormFieldModule,
+    //Router
+    RouterModule.forRoot(routes),
+    //Form
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  //need to add Export for Router Module
+  exports:[
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
